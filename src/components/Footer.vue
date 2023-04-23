dce9d7dd487298e256488567fb8abe1d1909350f
erdem <erdemhacisalihoglu@hotmail.com>
2023-04-23 19:51:16 +0300

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,20 +3,20 @@
     class="flex flex-col w-full p-16 bg-gradient-to-b bg-no-repeat bg-cover from-[#3d3a4b] to-[#1c1b24] gap-y-2"
   >
     <div class="max-w-4xl mx-auto text-center">
-      <h1 class="text-4xl font-bold text-white">Tamam, yalan söyledik.</h1>
+      <h1 class="text-4xl font-bold text-white">Okay, we lied.</h1>
       <p class="mt-3 text-xl font-medium text-gray-400">
-        Açıklaması o kadar da zor değilmiş, ama olsun! Artık her şeyi
-        biliyorsun.
+        It's not that hard to explain, but okay! Now you'll know everything
+        you know.
         <br />
-        Sen de Ritim'i sunucuna ekle, eğlenceye başla!
+        Add Ritim to your server and start having fun!
       </p>
       <a
         class="flex mt-6 items-center text-center mx-auto font-medium justify-center w-64 gap-2 p-2.5 text-xl text-gray-300 bg-[#17161d] rounded-xl shadow-md hover:shadow-lg transition duration-500"
-        href="https://discord.com/oauth2/authorize?client_id=711411719922253884&scope=bot&permissions=1110822208"
+        href="https://discord.com/api/oauth2/authorize?client_id=711411719922253884&permissions=57877971988288&scope=applications.commands%20bot"
         draggable="false"
         target="_blank"
       >
-        İkna oldum</a
+        Convinced</a
       >
     </div>
   </section>
